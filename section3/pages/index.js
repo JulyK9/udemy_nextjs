339a34cc0d5728e6289288ee2d58d099dc533c49
JKKIM <jkesonjk@gmail.com>
2023-01-06 21:49:23 +0900

    Link 컴포넌트로 네비게이션 처리

--- section3/pages/index.js
+++ section3/pages/index.js
@@ -1,7 +1,19 @@
+import Link from "next/link";
+
 const HomePage = () => {
   return (
     <div>
       <h1>The Home Page</h1>
+      <ul>
+        <li>
+          {/* <a href="/portfolio">Portfolio</a> */}
+          <Link href="/portfolio">Portfolio</Link>
+          {/* <Link replace href="/portfolio">Portfolio</Link> */}
+        </li>
+        <li>
+          <Link href="/clients">Clients</Link>
+        </li>
+      </ul>
     </div>
   );
 };
