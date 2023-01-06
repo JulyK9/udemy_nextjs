766ef5e055bbba7e699db27ab052d7d5486d25fd
JKKIM <jkesonjk@gmail.com>
2023-01-06 23:06:33 +0900

    프로그래밍 방식으로 네비게이하기

--- section3/pages/clients/[id]/index.js
+++ section3/pages/clients/[id]/index.js
@@ -5,9 +5,23 @@ const ClientProjectPage = () => {
 
   console.log(router.query); // 해당 경로까지의 쿼리에 접근할 수 있음
 
+  const loadProjectHandler = () => {
+    // load data..etc
+    // router.push 메서드로 Link의 기능을 함수화해서 사용할 수 있음
+    // router.push("/clients/max/projecta");
+    // router.push(`/clients/${router.query.id}/projecta`);
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: { id: router.query.id, clientprojectid: "projecta" },
+    });
+    // replace 메서드는 현재페이지를 대체할 때(이전 페이지로 돌아갈 수 없음)
+    // router.replace(`/clients/${router.query.id}/projecta`);
+  };
+
   return (
     <div>
       <h1>The Project of Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 };
