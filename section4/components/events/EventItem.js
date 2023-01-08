745c079a3470a5e2b8959b074171c79e3b855a1c
JKKIM <jkesonjk@gmail.com>
2023-01-09 01:25:39 +0900

    #1 style: 아이콘 추가 및 적용

--- section4/components/events/EventItem.js
+++ section4/components/events/EventItem.js
@@ -1,7 +1,9 @@
-import Link from "next/link";
 import classes from "./EventItem.module.css";
 // import styles from './EventItem.module.css'
 import Button from "../ui/Button";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 
 const EventItem = (props) => {
   const { title, image, date, location, id } = props;
@@ -23,16 +25,23 @@ const EventItem = (props) => {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{easyToReadDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Link href={exploreLink}>
-            <Button link={exploreLink}>Explore Event</Button>
-          </Link>
+          <div href={exploreLink}>
+            <Button link={exploreLink}>
+              <span>Explore Event</span>
+              <span className={classes.icon}>
+                <ArrowRightIcon />
+              </span>
+            </Button>
+          </div>
         </div>
       </div>
     </li>
