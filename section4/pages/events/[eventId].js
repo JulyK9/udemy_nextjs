05f4cf16a71ab32fc5686829a294459b24857a42
JKKIM <jkesonjk@gmail.com>
2023-01-09 12:49:45 +0900

    #1 feat: 이벤트 디테일 페이지 컴포넌트 연결 및 동적 라우팅 적용

--- section4/pages/events/[eventId].js
+++ section4/pages/events/[eventId].js
@@ -1,5 +1,33 @@
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/eventdetail/event-summary";
+import EventLogistics from "../../components/eventdetail/event-logistics";
+import EventContent from "../../components/eventdetail/event-content";
+
 const EventDetailPage = () => {
-  return <div>EventDetailPage</div>;
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
+  return (
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
+  );
 };
 
 export default EventDetailPage;
