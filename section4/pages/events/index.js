d727ee3de969c64e44bc1d02f481360380c8438f
JKKIM <jkesonjk@gmail.com>
2023-01-09 17:17:32 +0900

    #1 feat: 이벤트 필터링을 위한 마크업 및 스타일링

--- section4/pages/events/index.js
+++ section4/pages/events/index.js
@@ -1,11 +1,13 @@
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/EventList";
+import EventSearch from "../../components/events/EventSearch";
 
 const AllEventsPage = () => {
   const events = getAllEvents();
 
   return (
     <div>
+      <EventSearch />
       <EventList items={events} />
     </div>
   );
