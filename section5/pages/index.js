331da154480800cd60c137f20773308f1478b5d8
JKKIM <jkesonjk@gmail.com>
2023-01-11 02:33:09 +0900

    #5 feat: 데이터 페칭 문제시 사용할 수 있는 옵션 notFound, redirect

--- section5/pages/index.js
+++ section5/pages/index.js
@@ -19,11 +19,23 @@ function HomePage(props) {
 
 // Next.js가 이 컴포넌트 페이지를 사전 렌더링하기 전 데이터를 프리페치해야하므로 getStaticProps 함수 사용
 // 쉽게말해서 이 컴포넌트를 실행하기 전에 getStaticProps 함수 부분을 먼저 실행함
-export async function getStaticProps() {
+export async function getStaticProps(context) {
   console.log("(Re-)Generating..."); // 서버 사이드쪽이므로 터미널에 콘솔이 표시되는 걸 확인할 수 있음
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json"); // process.cwd() 는 현재 작업 디렉토리(프로젝트의 루트 디렉토리임에 유의!)
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/no-data", // 이 라우트로 리디랙션 시켜줌(예시로 정한 라우트)
+      },
+    };
+  }
+
+  if (data.products.length === 0) {
+    return { NotFound: true };
+  }
 
   // 이 함수는 항상 props를 key로 가지는 객체를 반환해야 함
   return {
