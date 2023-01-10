965447e4372a94acf984dff7fbbc53416cdce02a
JKKIM <jkesonjk@gmail.com>
2023-01-11 02:06:09 +0900

    #5 feat: ISR 설정 및 확인

--- section5/pages/index.js
+++ section5/pages/index.js
@@ -20,6 +20,7 @@ function HomePage(props) {
 // Next.js가 이 컴포넌트 페이지를 사전 렌더링하기 전 데이터를 프리페치해야하므로 getStaticProps 함수 사용
 // 쉽게말해서 이 컴포넌트를 실행하기 전에 getStaticProps 함수 부분을 먼저 실행함
 export async function getStaticProps() {
+  console.log("(Re-)Generating..."); // 서버 사이드쪽이므로 터미널에 콘솔이 표시되는 걸 확인할 수 있음
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json"); // process.cwd() 는 현재 작업 디렉토리(프로젝트의 루트 디렉토리임에 유의!)
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
@@ -30,6 +31,7 @@ export async function getStaticProps() {
       products: data.products,
     },
     // props: { products: [{ id: "p1", title: "product 1" }]},
+    revalidate: 10,
   };
 }
 
