9d2a5f27cac697e9bb7d4970b60711de3c6e9b56
JKKIM <jkesonjk@gmail.com>
2023-01-10 23:46:37 +0900

    #5 feat: getStaticProps 함수에 파일 경로 접근 로직 작성

--- section5/pages/index.js
+++ section5/pages/index.js
@@ -1,3 +1,6 @@
+import path from "path";
+import fs from "fs/promises";
+
 function HomePage(props) {
   const { products } = props;
   // console.log(products);
@@ -17,11 +20,16 @@ function HomePage(props) {
 // Next.js가 이 컴포넌트 페이지를 사전 렌더링하기 전 데이터를 프리페치해야하므로 getStaticProps 함수 사용
 // 쉽게말해서 이 컴포넌트를 실행하기 전에 getStaticProps 함수 부분을 먼저 실행함
 export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json"); // process.cwd() 는 현재 작업 디렉토리(프로젝트의 루트 디렉토리임에 유의!)
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
   // 이 함수는 항상 props를 key로 가지는 객체를 반환해야 함
   return {
     props: {
-      products: [{ id: "p1", title: "product 1" }],
+      products: data.products,
     },
+    // props: { products: [{ id: "p1", title: "product 1" }]},
   };
 }
 
