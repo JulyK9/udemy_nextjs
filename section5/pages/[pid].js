c875b21e3be4a204d30da4a5dfab59a896b3e340
JKKIM <jkesonjk@gmail.com>
2023-01-12 13:06:29 +0900

    #5 feat: fallback과 컴포넌트내 반환 설정

--- section5/pages/[pid].js
+++ section5/pages/[pid].js
@@ -4,6 +4,12 @@ import path from "path";
 const ProductDetailPage = (props) => {
   const { loadedproduct } = props;
 
+  // fallback 기능을 true 작동시키면 fallback 상태를 반환할 수 있게 해줘야 함
+  // if (!loadedproduct) {
+  //   return <p>Loading...</p>;
+  // }
+  // fallback 반환 확인을 할 필요가 없을 때는 지워줌
+
   return (
     <>
       <h1>{loadedproduct.title}</h1>
@@ -23,13 +29,25 @@ export async function getStaticProps(context) {
   const data = JSON.parse(jsonData);
 
   // 추출한 id로 product 필터링
-  const product = data.products.map((product) => product.id === productId);
+  const product = data.products.find((product) => product.id === productId);
 
   // props를 통해 컴포넌트에 전달하여 사용
   return {
     props: {
       loadedproduct: product,
     },
+  };
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { pid: "p1" } },
+      // { params: { pid: "p2" } }, // fallback true로 인해 추가하지 않아도 로딩은 됨(다만 사전 생성되는건 아님)
+      // { params: { pid: "p3" } },
+    ],
+    // fallback: true, // 일부 페이지만 사전 렌더링 하는 속성
+    fallback: blocking, // 에러가 나진 않지만 계속 기다려서 페이지를 온전하게 보여주게 하고 싶은 경우
   };
 }
 
