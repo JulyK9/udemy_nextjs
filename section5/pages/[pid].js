f29ef51adecbd3a91e923a51725cc68170a5332a
JKKIM <jkesonjk@gmail.com>
2023-01-13 11:04:30 +0900

    #5 feat: gestStaticPaths에서 페칭 데이터의 id를 통해 동적 경로 설정

--- section5/pages/[pid].js
+++ section5/pages/[pid].js
@@ -18,15 +18,21 @@ const ProductDetailPage = (props) => {
   );
 };
 
+async function getData() {
+  // 로컬에서 특정 데이터 페칭
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  return data;
+}
+
 export async function getStaticProps(context) {
   // getStaticProps 함수 안에서 context 매개변수를 통해 동적 경로 세그먼트에 접근하여 id 추출
   const { params } = context;
   const productId = params.pid;
 
-  // 로컬에서 특정 데이터 페칭
-  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
-  const jsonData = await fs.readFile(filePath);
-  const data = JSON.parse(jsonData);
+  const data = await getData();
 
   // 추출한 id로 product 필터링
   const product = data.products.find((product) => product.id === productId);
@@ -40,14 +46,21 @@ export async function getStaticProps(context) {
 }
 
 export async function getStaticPaths() {
+  // 실제 페칭한 데이터의 id를 통해 동적으로 경로를 설정
+  const data = await getData();
+  const ids = data.products.map((product) => product.id);
+  const pathWithParams = ids.map((id) => ({ params: { pid: id } }));
+
   return {
-    paths: [
-      { params: { pid: "p1" } },
-      // { params: { pid: "p2" } }, // fallback true로 인해 추가하지 않아도 로딩은 됨(다만 사전 생성되는건 아님)
-      // { params: { pid: "p3" } },
-    ],
+    paths: pathWithParams,
+    // paths: [
+    //   { params: { pid: "p1" } }, // 하드코딩
+    //   // { params: { pid: "p2" } }, // fallback true로 인해 추가하지 않아도 로딩은 됨(다만 사전 생성되는건 아님)
+    //   // { params: { pid: "p3" } },
+    // ],
+    fallback: false,
     // fallback: true, // 일부 페이지만 사전 렌더링 하는 속성
-    fallback: blocking, // 에러가 나진 않지만 계속 기다려서 페이지를 온전하게 보여주게 하고 싶은 경우
+    // fallback: blocking, // 에러가 나진 않지만 계속 기다려서 페이지를 온전하게 보여주게 하고 싶은 경우
   };
 }
 
