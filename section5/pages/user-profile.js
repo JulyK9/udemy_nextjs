acbe7df8cb1d39df44ef59e7ae4e9dbfc82a0c67
JKKIM <jkesonjk@gmail.com>
2023-01-17 11:29:23 +0900

    #5 chore: getServerSideProps 구현 페이지 빌드 모드 확인

--- section5/pages/user-profile.js
+++ section5/pages/user-profile.js
@@ -10,6 +10,9 @@ export async function getServerSideProps(context) {
   // console.log(req);
   // console.log(res);
 
+  // build 모드를 통해서 서버사이드에서(터미널) 출력은 되지만 페이지는 사전생성된 것이 아님을 알 수 있음
+  console.log("server side code test");
+
   return {
     props: {
       username: "Max",
