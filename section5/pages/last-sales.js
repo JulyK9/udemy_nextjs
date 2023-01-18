f2cccf66fac6b63a083716b9c1ffa6b7765cdafa
JKKIM <jkesonjk@gmail.com>
2023-01-18 22:29:56 +0900

    #5 feat: 사전 데이터 페칭과 클라이언트 사이드 데이터 페칭과 결합

--- section5/pages/last-sales.js
+++ section5/pages/last-sales.js
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import useSWR from "swr";
 
-const LastSalesPage = () => {
-  const [sales, setSales] = useState();
+const LastSalesPage = (props) => {
+  // getStaticProps 로부터 서버에서 받은 혹은 빌드 프로세스 중에 사전 렌더링된 이 sales가
+  // 바로 초기 상태로 사용되는 sales이며
+  const [sales, setSales] = useState(props.sales);
   // const [isLoading, setIsLoading] = useState(false);
 
+  // 클라이언트 사이드 데이터 페칭의 결과를 덮어쓰게 될 것
   const fetcher = (url) => fetch(url).then((r) => r.json());
 
   const { data, error } = useSWR(
@@ -57,7 +60,8 @@ const LastSalesPage = () => {
     return <p>Failed to load</p>;
   }
 
-  if (!data || !sales) {
+  // if (!data || !sales) {
+  if (!data && !sales) {
     return <p>Loading...</p>;
   }
 
@@ -75,5 +79,30 @@ const LastSalesPage = () => {
     </ul>
   );
 };
+
+export async function getStaticProps() {
+  // 모든 단계 및 최종 데이터를 가지는 전체 프로미스가 getStaticProps로 반환되도록 전체 프로미스 체인을 return
+
+  const response = await fetch(
+    "https://nextjs-course-e6529-default-rtdb.firebaseio.com/sales.json"
+  );
+  const data = await response.json();
+
+  // 객체 형태의 sales 데이터를 배열로 변경
+  const transformedSales = [];
+
+  for (const key in data) {
+    transformedSales.push({
+      id: key,
+      username: data[key].username,
+      volume: data[key].volume,
+    });
+  }
+  // async 비동기 함수이므로 promise 객체가 리턴되는 2번째 then 블록 안에서 객체를 return 해야함
+  return {
+    // props: { sales: transformedSales, revalidate: 10 },
+    props: { sales: transformedSales },
+  };
+}
 
 export default LastSalesPage;
