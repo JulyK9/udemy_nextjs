6feabd8dd91eb99aa2a7cd247571ed244c68fc1d
JKKIM <jkesonjk@gmail.com>
2023-01-18 11:06:42 +0900

    #5 feat: 클라리언트 사이드 데이터 페칭 기본 작성

---
+++ section5/pages/last-sales.js
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+
+const LastSalesPage = () => {
+  const [sales, setSales] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch("https://nextjs-course-e6529-default-rtdb.firebaseio.com/sales.json")
+      .then((res) => res.json())
+      .then((data) => {
+        // 객체 형태의 sales 데이터를 배열로 변경
+        const transformedSales = [];
+
+        for (const key in data) {
+          transformedSales.push({
+            id: key,
+            username: data[key].username,
+            volume: data[key].volume,
+          });
+        }
+
+        setSales(transformedSales);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (!sales) {
+    return <p>No data yet</p>;
+  }
+
+  return (
+    <ul>
+      {sales.map((sale) => (
+        <li key={sale.id}>
+          {sale.username} - ${sale.volume}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default LastSalesPage;
