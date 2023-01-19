8cc03c08f93292d698f55cd739914a3e8c6598f0
JKKIM <jkesonjk@gmail.com>
2023-01-19 11:43:23 +0900

    #8 feat: 메인페이지 부분 외부데이터 페칭하면서 getStaticProps로 사전 페이지 렌더링 작업 구현

--- section6/pages/index.js
+++ section6/pages/index.js
@@ -1,14 +1,45 @@
-import { getFeaturedEvents } from '../dummy-data';
-import EventList from '../components/events/event-list';
+import { getFeaturedEvents } from "../dummy-data";
+import EventList from "../components/events/event-list";
 
-function HomePage() {
-  const featuredEvents = getFeaturedEvents();
-
+function HomePage(props) {
+  const { events } = props;
+  // const featuredEvents = getFeaturedEvents();
+  console.log(events);
   return (
     <div>
-      <EventList items={featuredEvents} />
+      {/* <EventList items={featuredEvents} /> */}
+      <EventList items={events} />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(
+    "https://nextjs-course-e6529-default-rtdb.firebaseio.com/events.json"
+  );
+
+  const data = await res.json();
+
+  // console.log(data); // 가져오는 데이터 형태 확인
+
+  const transformedData = [];
+
+  for (const key in data) {
+    transformedData.push({
+      id: key,
+      location: data[key].location,
+      title: data[key].title,
+      description: data[key].description,
+      image: data[key].image,
+      date: data[key].date,
+    });
+  }
+
+  // console.log(transformedData); // 변형된 데이터 확인
+
+  return {
+    props: { events: transformedData },
+  };
 }
 
 export default HomePage;
