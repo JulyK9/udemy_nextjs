eabac2377e99759489c81f2a2e0d7a3fd5f53243
JKKIM <jkesonjk@gmail.com>
2023-01-19 11:59:42 +0900

    #8 feat: useSWR을 활용하여 데이터 변동시 클라이언트 사이드 데이터 페칭 추가

--- section6/pages/index.js
+++ section6/pages/index.js
@@ -1,10 +1,39 @@
 import { getFeaturedEvents } from "../dummy-data";
 import EventList from "../components/events/event-list";
+import useSWR from "swr";
+import { useEffect, useState } from "react";
 
 function HomePage(props) {
-  const { events } = props;
+  // const { events } = props;
   // const featuredEvents = getFeaturedEvents();
-  console.log(events);
+  // console.log(events);
+  const [events, setEvents] = useState(props.events);
+
+  const fetcher = (url) => fetch(url).then((res) => res.json());
+
+  const { data, error } = useSWR(
+    "https://nextjs-course-e6529-default-rtdb.firebaseio.com/events.json",
+    fetcher
+  );
+
+  useEffect(() => {
+    if (data) {
+      const transformedData = [];
+
+      for (const key in data) {
+        transformedData.push({
+          id: key,
+          location: data[key].location,
+          title: data[key].title,
+          description: data[key].description,
+          image: data[key].image,
+          date: data[key].date,
+        });
+      }
+      setEvents(transformedData);
+    }
+  }, [data]);
+
   return (
     <div>
       {/* <EventList items={featuredEvents} /> */}
