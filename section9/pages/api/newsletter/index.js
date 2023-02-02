cd6aa12045ebcf301bb9311e32f258533dce7ec0
JKKIM <jkesonjk@gmail.com>
2023-02-02 14:08:34 +0900

    #16 feat: api 라우트 내부에서 mongodb 연결 및 데이터 연결작업 테스트

--- section9/pages/api/newsletter/index.js
+++ section9/pages/api/newsletter/index.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const userEmail = req.body.email;
 
@@ -12,7 +14,22 @@ function handler(req, res) {
       return;
     }
 
-    console.log(userEmail);
+    // connect 메서드로 mongodb client에 연결, 연결할 클러스터는 주소를 복사해서 사용
+    // id와 pw는 database access 에서 정하고 network access 에서 ip도 허용해줄 것
+    const client = await MongoClient.connect(
+      "mongodb+srv://udemy_jk929jk:roqkfwk929!@boilerplate.axivy.mongodb.net/?retryWrites=true&w=majority"
+    );
+
+    // mongodb 클라이언트의 db에 접근(db네임)
+    const db = client.db("newsletter");
+
+    // db에 컬렉션에 접근하여(collection 메서드) 객체를 삽입(insertOne 메서드)
+    await db.collection("emails").insertOne({ email: userEmail });
+
+    // 작업후 클라이언트 닫기
+    client.close();
+
+    // console.log(userEmail);
     res.status(201).json({ message: `${userEmail} Sign up Success!` });
   }
 }
