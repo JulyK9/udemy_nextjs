c76e9f9bfa08b292ff5ed416d8a23c59355733ba
JKKIM <jkesonjk@gmail.com>
2023-02-15 12:49:47 +0900

    #21 feat: 랜딩 컴포넌트 작업, Image 컴포넌트로 이미지 렌더 적용

--- section11/components/home-page/hero.js
+++ section11/components/home-page/hero.js
@@ -1,9 +1,18 @@
+import Image from "next/image";
 import classes from "./hero.module.css";
 
 const Hero = () => {
   return (
     <section className={classes.hero}>
-      <div className={classes.image}>Image</div>
+      <div className={classes.image}>
+        {/* <Image src="/section11/public/images/site/latte-art.jpg" alt="An image showing me" width={ } height={ } /> */}
+        <Image
+          src="/images/site/latte-art.jpg"
+          alt="An image showing me"
+          width={300}
+          height={300}
+        />
+      </div>
       <h1>Hello, I'm JK</h1>
       <p>I blog about web frontend development</p>
     </section>
--- section11/pages/index.js
+++ section11/pages/index.js
@@ -1,5 +1,13 @@
+// import FeaturedPosts from "../components/home-page/featured-posts";
+import Hero from "../components/home-page/hero";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <Hero />
+      {/* <FeaturedPosts />; */}
+    </>
+  );
 };
 
 export default HomePage;
