e63f44d2e5efe08d26cc86edb1fafb3413e2ab9c
JKKIM <jkesonjk@gmail.com>
2023-02-16 15:31:02 +0900

    #21 feat: 더미 게시물 데이터로 개별 포스트 렌더링

--- section11/components/home-page/featured-posts.js
+++ section11/components/home-page/featured-posts.js
@@ -1,9 +1,12 @@
+import PostGrid from "../posts/posts-grid";
 import classes from "./featured-posts.module.css";
 
-const FeaturedPosts = () => {
+const FeaturedPosts = (props) => {
   return (
     <section className={classes.latest}>
-      <h2>Featured Posts</h2>A list of Posts
+      <h2>Featured Posts</h2>
+      {/* A list of Posts */}
+      <PostGrid posts={props.posts} />
     </section>
   );
 };
--- section11/pages/index.js
+++ section11/pages/index.js
@@ -2,10 +2,47 @@ import FeaturedPosts from "../components/home-page/featured-posts";
 import Hero from "../components/home-page/hero";
 
 const HomePage = () => {
+  const DUMMY_POSTS = [
+    {
+      title: "Getting Started with NextJS",
+      image: "getting-started-nextjs.png",
+      excerpt:
+        "NextJS is a React framework for production - it makes building fullstack React apps and sites a breeze and ships with build-in SSR",
+      date: "2023-02-15",
+      slug: "getting-started-with-nextjs1",
+    },
+    {
+      title: "Getting Started with NextJS",
+      image: "getting-started-nextjs.png",
+      excerpt:
+        "NextJS is a React framework for production - it makes building fullstack React apps and sites a breeze and ships with build-in SSR",
+      date: "2023-02-15",
+      slug: "getting-started-with-nextjs2",
+    },
+    {
+      title: "Getting Started with NextJS",
+      image: "getting-started-nextjs.png",
+      excerpt:
+        "NextJS is a React framework for production - it makes building fullstack React apps and sites a breeze and ships with build-in SSR",
+      date: "2023-02-15",
+      slug: "getting-started-with-nextjs3",
+    },
+    {
+      title: "Getting Started with NextJS",
+      image: "getting-started-nextjs.png",
+      excerpt:
+        "NextJS is a React framework for production - it makes building fullstack React apps and sites a breeze and ships with build-in SSR",
+      date: "2023-02-15",
+      slug: "getting-started-with-nextjs4",
+    },
+  ]; // 더미 게시물 객체들의 배열
+
   return (
     <>
       <Hero />
-      <FeaturedPosts />;
+      {/* <FeaturedPosts /> */}
+      {/* getStaticProps 를 통해 데이터를 가져와서 props로 하위 컴포넌트에 전달 */}
+      <FeaturedPosts posts={DUMMY_POSTS} />
     </>
   );
 };
