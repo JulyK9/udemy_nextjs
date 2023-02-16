d49ab19243aa133b0857c0bf73e65d976827ad4b
JKKIM <jkesonjk@gmail.com>
2023-02-16 16:45:45 +0900

    #21 feat: 전체 포스트 페이지 작업

--- section11/pages/posts/index.js
+++ section11/pages/posts/index.js
@@ -1,10 +1,42 @@
+import AllPosts from "../../components/posts/all-posts";
+
 const AllPostsPage = () => {
-  return (
-    <>
-      <Hero />
-      <FeaturedPosts />
-    </>
-  );
+  const DUMMY_POSTS = [
+    {
+      title: "Getting Started with NextJS",
+      image: "getting-started-nextjs.png",
+      excerpt:
+        "NextJS is a React framework for production - it makes building fullstack React apps and sites a breeze and ships with build-in SSR",
+      date: "2023-02-15",
+      slug: "getting-started-with-nextjs1",
+    },
+    {
+      title: "Getting Started with NextJS",
+      image: "getting-started-nextjs.png",
+      excerpt:
+        "NextJS is a React framework for production - it makes building fullstack React apps and sites a breeze and ships with build-in SSR",
+      date: "2023-02-15",
+      slug: "getting-started-with-nextjs2",
+    },
+    {
+      title: "Getting Started with NextJS",
+      image: "getting-started-nextjs.png",
+      excerpt:
+        "NextJS is a React framework for production - it makes building fullstack React apps and sites a breeze and ships with build-in SSR",
+      date: "2023-02-15",
+      slug: "getting-started-with-nextjs3",
+    },
+    {
+      title: "Getting Started with NextJS",
+      image: "getting-started-nextjs.png",
+      excerpt:
+        "NextJS is a React framework for production - it makes building fullstack React apps and sites a breeze and ships with build-in SSR",
+      date: "2023-02-15",
+      slug: "getting-started-with-nextjs4",
+    },
+  ]; // 더미 게시물 객체들의 배열
+
+  return <AllPosts posts={DUMMY_POSTS} />;
 };
 
 export default AllPostsPage;
