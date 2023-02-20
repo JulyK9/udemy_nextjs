9d0149ecd5eeb2d078004e9362f7bdb65382a11f
JKKIM <jkesonjk@gmail.com>
2023-02-20 12:01:00 +0900

    #21 feat: 사전생성한 데이터 props로 컴포넌트에 전달하여 렌더

--- section11/components/posts/post-detail/post-content.js
+++ section11/components/posts/post-detail/post-content.js
@@ -2,22 +2,28 @@ import ReactMarkdown from "react-markdown";
 import classes from "./post-content.module.css";
 import PostHeader from "./post-header";
 
-const DUMMY_POST = {
-  title: "Getting Started with NextJS",
-  image: "getting-started-nextjs.png",
-  content: "# This is a first ",
-  date: "2023-02-15",
-  slug: "getting-started-with-nextjs1",
-};
+// const DUMMY_POST = {
+//   title: "Getting Started with NextJS",
+//   image: "getting-started-nextjs.png",
+//   content: "# This is a first ",
+//   date: "2023-02-15",
+//   slug: "getting-started-with-nextjs1",
+// };
 
-const PostContent = () => {
-  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+const PostContent = (props) => {
+  const { post } = props;
+  // console.log("post: ", post);
+
+  // const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
   return (
     <article className={classes.content}>
-      <PostHeader title={DUMMY_POST.title} image={imagePath} />
+      {/* <PostHeader title={DUMMY_POST.title} image={DUMMY_POST.imagePath} /> */}
+      <PostHeader title={post.title} image={imagePath} />
       {/* <p>{DUMMY_POST.content}</p> */}
-      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+      {/* <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown> */}
+      <ReactMarkdown>{post.content}</ReactMarkdown>
     </article>
   );
 };
