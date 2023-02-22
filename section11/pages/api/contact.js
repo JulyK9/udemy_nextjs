f85ec4c80af63de126bd0cd8da522374da565c72
JKKIM <jkesonjk@gmail.com>
2023-02-22 15:18:31 +0900

    #21 feat: mongoDB 설치 및 api 라우터에서 프론트에서 받은 데이터를 mongoDB 컬렉션에 저장해주는 로직 작성

--- section11/pages/api/contact.js
+++ section11/pages/api/contact.js
@@ -1,6 +1,7 @@
 // /api/contact 형식의 구조로 추가
+import { MongoClient } from "mongodb";
 
-function handler(req, res) {
+async function handler(req, res) {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
 
@@ -23,7 +24,33 @@ function handler(req, res) {
       message,
     };
 
-    console.log(newMessage);
+    // console.log(newMessage);
+
+    let client;
+
+    try {
+      // MongoDB 데이터베이스에 연결
+      client = await MongoClient.connect(
+        "mongodb+srv://udemy_jk929jk:udemyjk929jk@boilerplate.axivy.mongodb.net/my-site?retryWrites=true&w=majority"
+      );
+    } catch (error) {
+      res.status(500).json({ message: "Could not connect to database." });
+      return;
+    }
+
+    const db = client.db("my-site"); // db에 엑세스
+
+    try {
+      // db의 컬렉션에서 작업
+      const result = await db.collection("messages").insertOne(newMessage);
+      newMessage.id = result.insertedId; // db에 메시지가 삽입된 이후 자동 생성된 id를 메시지 객체에 삽입
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: "Storing message failed!" });
+      return;
+    }
+
+    client.close(); // 최종 성공하고나서도 db를 닫아주어야함 유의!
 
     res.status(201).json({ message: "Succesfully stored message!" });
   }
