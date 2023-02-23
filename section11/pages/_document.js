89084cb943f5641ae38ca73040b88aad107450b4
JKKIM <jkesonjk@gmail.com>
2023-02-23 15:25:06 +0900

    #21 feat: document.js 파일과 React Portal을 사용해서 별도의 dom 트리로 시멘틱한 렌더링 코드로 개선

--- section11/pages/_document.js
+++ section11/pages/_document.js
@@ -8,6 +8,8 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          {/* Notification 컴포넌트가 렌더될 때 여기로 연결되도록 하는 사전 작업 */}
+          <div id="notifications"></div>
         </body>
       </Html>
     );
